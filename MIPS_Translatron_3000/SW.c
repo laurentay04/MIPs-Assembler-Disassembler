21e1ff91b0fb186ae19051605e903d6cbc51076e
Untamed1737 <110755095+Untamed1737@users.noreply.github.com>
2025-11-06 15:58:01 -0500

    Completed SW (needs testing) - Aliaksei Sapatsko

--- MIPS_Translatron_3000/SW.c
+++ MIPS_Translatron_3000/SW.c
@@ -3,6 +3,7 @@
 * Date: 06/13/2012
 * ByteForge Systems
 * MIPS-Translatron 3000
+* Edited By: Aliaksei Sapatsko
 */
 
 #include "Instruction.h"
@@ -45,29 +46,34 @@ void sw_immd_assm(void) {
 	// setBits_num(25, PARAM2.value, 16);
 	// setBits_num(20, PARAM3.value, 5);
 
-	setBits_str(31, "101011");
-	setBits_num(20, PARAM1.value, 5);
-	setBits_num(15, PARAM2.value, 16);
-	setBits_num(25, PARAM3.value, 5);
+	// I Format: op, rs, rt, immediate
+
+	setBits_str(31, "101011"); //opcode
+	setBits_num(20, PARAM1.value, 5); //rt (what we're storing)
+	setBits_num(15, PARAM2.value, 16); //offset/immediate
+	setBits_num(25, PARAM3.value, 5); //rs (target address)
 
 	state = COMPLETE_ENCODE;
 }
 
 void sw_immd_bin(void) {
 
+	// check opcode bits
 	if (checkBits(31, "101011") != 0) {
 		state = WRONG_COMMAND;
 		return;
 	}
 
+	// find values in binary
 	uint32_t Rs = getBits(25, 5);
 	uint32_t Rt = getBits(20, 5);
 	uint32_t offset = getBits(15, 16);
 
+	// set instruction values 
 	setOp("SW");
 	setParam(1, REGISTER, Rt); 
-	setParam(2, REGISTER, Rs); 
-	setParam(3, IMMEDIATE, offset); 
+	setParam(2, IMMEDIATE, offset); //offset is param 2
+	setParam(3, REGISTER, Rs); 
 
 	state = COMPLETE_DECODE;
 }
