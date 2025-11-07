bf166eaf3c7dfa0ceeb469bb4e7e184060c7fb77
Matthew Coffman <mwc00007@mix.wvu.edu>
2025-11-06 19:13:40 -0500

    Update LW.c

--- MIPS_Translatron_3000/LW.c
+++ MIPS_Translatron_3000/LW.c
@@ -20,13 +20,13 @@ void lw_immd_assm(void) {
 	/*
 		Checking the type of parameters
 	*/
-	// The first parameter should be a register
+	// Param 1 should be a register
 	if (PARAM1.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
-	// Param 2 needs to be a register
-	if (PARAM2.type != REGISTER) {
+	// Param 2 needs to be a register	// Parameters were not in the correct Order, 
+	if (PARAM2.type != REGISTER) {		// Now Register field parameter is checked accordingly
 		state = MISSING_REG;
 		return;
 	}
@@ -43,20 +43,20 @@ void lw_immd_assm(void) {
 		return;
 	}
 	// Rs should be 31 or less
-	if ( PARAM2.value > 31) {
+	if ( PARAM2.value > 31) {			// Because Param 1 and 2 are the register fields (5 bits) they need to be less then 32
 		state = INVALID_REG;
 		return;
 	}
 	// The immediate value is limited to 16 bits, this is 0xFFFF
-	if ( PARAM2.value > 0xFFFF) {
-		state = INVALID_IMMED;
-		return;
+	if ( PARAM3.value > 0xFFFF) {
+		state = INVALID_IMMED;			// Because Param 3 is an immediate field, it needs to be less than 65535 or "1111 1111 1111 1111" 
+		return;							// 0x7FFF != 65535 			0xFFFF = 65535
 	}
 	
 	//encode instruction 
 	setBits_str(31, "100011");
 	setBits_num(20, PARAM1.value, 5);
-	setBits_num(25, PARAM2.value, 5);
+	setBits_num(25, PARAM2.value, 5);		// Param 2 is the reg field, therefore 2 and 3 needed to be swapped
 	setBits_num(15, PARAM3.value, 16);
 
 	// tell the system the encoding is done
@@ -84,10 +84,11 @@ void lw_immd_bin(void) {
 	*/
 	setOp("LW");
 	setParam(1, REGISTER, Rt);
-	setParam(2, REGISTER, Rs);
+	setParam(2, REGISTER, Rs);			// Param 2 is the reg field, therefore 2 and 3 needed to be swapped
 	setParam(3, IMMEDIATE, imm16);
 
 	// tell the system the decoding is done
 	state = COMPLETE_DECODE;
 
 }
+
